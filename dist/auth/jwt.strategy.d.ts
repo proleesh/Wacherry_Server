18b58ea393754bf11053f2495c50421cab7fb6a0
Sung-Hyuk Lee <merci726@yahoo.com>
2025-04-01 19:46:50 +0900

    update about nestjs version 10 to 11 and adding logic about mov format

--- dist/auth/jwt.strategy.d.ts
+++ dist/auth/jwt.strategy.d.ts
@@ -1,4 +1,4 @@
-declare const JwtStrategy_base: new (...args: any[]) => any;
+declare const JwtStrategy_base: new (...args: any) => any;
 export declare class JwtStrategy extends JwtStrategy_base {
     constructor();
     validate(payload: any): Promise<{
